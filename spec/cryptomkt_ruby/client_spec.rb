648ebf38c7969f9b7f410927ee027a9bf04d06fe
Gonzalo Moreno <goncab380@hotmail.com>
2018-07-24 23:59:53 -0400

    added public API tests with vcr

--- spec/cryptomkt_ruby/client_spec.rb
+++ spec/cryptomkt_ruby/client_spec.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 RSpec.describe CryptomktRuby::Client do
   it 'has a version number' do
     expect(CryptomktRuby::VERSION).not_to be nil
@@ -30,6 +32,46 @@ RSpec.describe CryptomktRuby::Client do
       expect(
         subject.send(:signature, timestamp, path, params)
       ).not_to eq expected_message
+    end
+  end
+
+  context 'public endpoints', :vcr do
+    subject { CryptomktRuby::Client.new('some_key', 'super_secure_secret') }
+
+    it 'get cryptomkt markets' do
+      expect(subject.market.size).to eq 12
+    end
+
+    context 'ticker with market specified' do
+      it 'get one cryptomkt ticker' do
+        expect(subject.ticker(market: "ETHCLP").size).to eq 1
+      end
+    end
+
+    context 'ticker without market specified' do
+      it 'get all cryptomkt ticker' do
+        expect(subject.ticker.size).to eq 12
+      end
+    end
+
+    context 'book requests' do
+      it 'get default page size if limit not sent' do
+        expect(subject.book(market: "ETHCLP", type: :buy).size).to eq 20
+      end
+
+      it 'get custom page size if limit sent' do
+        expect(subject.book(market: "ETHCLP", type: :buy, limit: 5).size).to eq 5
+      end
+    end
+
+    context 'trade requests' do
+      it 'get default page size if limit not sent' do
+        expect(subject.trades(market: "ETHCLP", start_at: (Date.today-1)).size).to eq 20
+      end
+
+      it 'get custom page size if limit sent' do
+        expect(subject.trades(market: "ETHCLP", start_at: (Date.today-1), limit: 5).size).to eq 5
+      end
     end
   end
 end
